e0eb90dea0c93dd3ac893f42924107880a7809d9
Jacob Fifield <43118015+Fife@users.noreply.github.com>
2023-09-28 16:24:10 -0600

    Update BT-Test.cpp

--- BT-Module/BT-Test.cpp
+++ BT-Module/BT-Test.cpp
@@ -1,66 +1,97 @@
 #include <Arduino.h>
+#include <string>
 
-//BT Pin Setup
+//Pin definitions
 #define WAKEUP 30
 #define INT 34
 #define PDN 35
 #define BT_SERIAL Serial1
-#define USB_SERIAL Serial
 
-void requestName(void);
+//Globals
+char BUFFER[256];
 
-void setup() {
-    // initialize both serial ports:
-    USB_SERIAL.begin(9600);
+//Function Declarations
+int BTinit();
+void getName(char *);
+void wakeBT();
+bool isOK(char *);
+int rollLeaderNumber();
+
+//Function Implementations
+int BTinit(){
     BT_SERIAL.begin(115200);
+    //Setup BT Pins
     pinMode(WAKEUP, OUTPUT);
     pinMode(PDN, OUTPUT);
     pinMode(INT, INPUT);
 
     //Exit Sleep Mode
     digitalWrite(PDN, HIGH);
-    delay(10);
     digitalWrite(PDN, LOW);
-
-    //Wake up the module by sending a falling edge
-    digitalWrite(WAKEUP, HIGH);
-    digitalWrite(WAKEUP, LOW);
     delay(1);
-    char renameCommand[] = "TTM:REN-TellusBot-1\r\n\0";
+
+    wakeBT();
+    //char* nameBuffer[24];
+    //char leaderNumber = (char)rollLeaderNumber();
+    //char trailingChars[] = "\r\n\0";
+    //char renameCommand[] = "TTM:REN-TellusBot-";
+
+    //Assign A name to the module. 
+    char renameCommand[] = "TTM:REN-TellusBot-654\r\n\0";
     if(BT_SERIAL.availableForWrite()){
-        for (int i = 0; i < 22; i++){
+        for (int i = 0; i < sizeof(renameCommand); i++){
             BT_SERIAL.write(renameCommand[i]);
         }
     }
-}
 
-void loop()
-{
-    requestName();
-
-    // Write the incoming characters to the USB Serial
+    delay(10);
+    int i = 0;
+    // Write the incoming characters to the Buffer
     while(BT_SERIAL.available()){
-        char current = (char)BT_SERIAL.read();
-        if (current != '\0'){
-            USB_SERIAL.write(current);
-        }
-    }
-
-    delay(1000);
+        BUFFER[i] = (char)BT_SERIAL.read();
+        i++;
+    } 
+    return isOK(BUFFER);
 }
 
-void requestName(void){
-    //Wake up the module by sending a falling edge
-    digitalWrite(WAKEUP, HIGH);
-    digitalWrite(WAKEUP, LOW);
-    delay(1);
-
+void getName(char * output){
+    wakeBT();
     //Ask it it's name
     char strCommand[] = "TTM:NAM-?\r\n\0";
     if(BT_SERIAL.availableForWrite()){
         for (int i = 0; i < 12; i++){
             BT_SERIAL.write(strCommand[i]);
         }
-        USB_SERIAL.println(strCommand);
     }
+    int i = 0;
+    // Write the incoming characters to the Buffer
+    while(BT_SERIAL.available()){
+        output[i] = (char)BT_SERIAL.read();
+        i++;
+    } 
+}
+
+void wakeBT(){
+    //Wake up the module by sending a falling edge
+    digitalWrite(WAKEUP, HIGH);
+    digitalWrite(WAKEUP, LOW);
+    delay(1);
+}
+
+bool isOK(char * btResponse) {
+    // Define the is okay string
+    const char* globalSubstring = "TTM:OK";
+
+    // Search for the is okay string in the response
+    const char* result = strstr(btResponse, globalSubstring);
+
+    // Check if the substring is found.
+    return (result != NULL); 
+}
+
+int rollLeaderNumber(){
+    //Randomly roll a leader number based off of a random seed
+    //Sampeled from an unconnected ADC pin.
+    randomSeed(analogRead(7));
+    return random(0,255);
 }
