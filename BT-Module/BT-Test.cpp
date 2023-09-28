9a17b40f81416b0e790403b474f286e8e539a10e
Jacob Fifield <43118015+Fife@users.noreply.github.com>
2023-09-28 16:25:43 -0600

    Create BT-Test.cpp

---
+++ BT-Module/BT-Test.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include <BT4502.cpp>
+#define USB_SERIAL Serial
+
+void setup() {
+  // initialize both serial ports:
+  USB_SERIAL.begin(9600);
+
+  if (BTinit()){ USB_SERIAL.println("Bluetooth setup succesfully"); }
+  else{ USB_SERIAL.println("An issue occured with Bluetooth Setup"); }
+}
+
+void loop()
+{
+  char name[25]; 
+  getName(name);
+  USB_SERIAL.print(name);
+  delay(1000);
+}
